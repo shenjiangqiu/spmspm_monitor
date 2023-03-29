434e4e91899f0f3d9551f05477f9bcc2df558bb7
Jiangqiu Shen <ivy22233qiu@live.com>
2023-03-29 17:49:36 -0400

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,7 @@
 pub mod monitor;
 use serde::Serialize;
-use std::sync::RwLock;
 #[derive(Debug, Default, Serialize, Clone)]
 pub struct Monitor {
     pub current_running_processes: usize,
     pub total_finished_process: usize,
-}
-
-lazy_static::lazy_static! {
-    pub static ref MONITOR: RwLock<Monitor> = RwLock::new(Monitor::default());
 }
